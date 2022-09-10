c6605f9f4426477eb3dfe37c3b1fcbf71645e134
WillamesLima01 <112348072+WillamesLima01@users.noreply.github.com>
2022-09-10 18:28:21 -0300

    Delete Aula_05 directory

--- Aula_05/Aula5_Q2.py
+++
@@ -1,6 +0,0 @@
-valor = (float(input("Digite um número: ")))
-
-if valor >= 0:
-    print(f'O número {valor:.0f} é positivo')
-else:
-    print(f'O número {valor:.0f} é negativo')
--- Aula_05/Aula5_Q4.py
+++
@@ -1,9 +0,0 @@
-avl1 = (int(input("Informe a nota da primeira avaliação: ")))
-avl2 = (int(input("Informe a nota da segunda avaliação: ")))
-
-media = (avl1 + avl2) / 2
-
-if media >= 6:
-    print(f'Parabéns, você foi aprovado(a) com média {media}')
-else:
-    print(f'Infelizmente sua média ficou abaixo do resultado necessário para aprovação. Média {media}')
