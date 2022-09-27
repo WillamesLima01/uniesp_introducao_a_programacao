f414919aa66a2377ff5475d94baf5f99ef998160
WillamesLima01 <112348072+WillamesLima01@users.noreply.github.com>
2022-09-26 23:49:08 -0300

    Delete Aula07_Q2.py

--- Aula07_Q2.py
+++
@@ -1,7 +0,0 @@
-ano_atual = (int(input("informe o ano atual: ")))
-ano_nasc = (int(input("informe o ano que você nasceu: ")))
-
-if ano_atual - ano_nasc < 18:
-    print("Você não pode votar nessa eleição!")
-else:
-    print("Parabéns! você pode votar nessa eleição")
